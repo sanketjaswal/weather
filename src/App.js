8e36993a5ca3c68a3724b34494c8dc494e621791
sanketjaswal <sanket.jaswal123@gmail.com>
2021-11-03 21:59:17 +0530

    more updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Nav } from "./Components/Nav";
 
 function App() {
-  async function getWeather() {
+  const slotHolder = [];
+  const [slot, setSlot] = useState(slotHolder);
+
+  useEffect(() => {
+    console.log(slot);
+  }, [slot]);
+
+  async function getWeather(city) {
     let response = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=3ef2ff326326b8bc60608844658645e4"
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=33a10730e6ca0c3509477c6f462add13`
     );
     response = await response.json();
-    // const Quote = response.data.quote;
-    // const Character = response.data.character.trim();
 
-    // document.getElementById("Quote").innerHTML = Quote;
-    // document.getElementById("Name").innerHTML = "- " + Character;
-    console.log(response);
+    const weather = response.weather[0].main;
+    const weatherExplain = response.weather[0].description;
+    const tempKelvin = response.main.temp;
+    const country = response.sys.country;
+    const longitute = response.coord.lon;
+    const latitute = response.coord.lat;
+    const tempCelsius = tempKelvin - 273.15;
+
+    console.log(weather);
+    console.log(weatherExplain);
+    console.log(tempKelvin);
+    console.log(tempCelsius);
+    console.log(country);
+    console.log(longitute);
+    console.log(latitute);
   }
 
+  const createSlot = () => {
+    setSlot((pre) => [...pre, ""]);
+  };
+
   const onSearch = () => {
-    console.log(document.getElementsByClassName("cityName")[0].value);
+    let city = document.getElementsByClassName("cityName")[0].value;
+    getWeather(city);
+    createSlot();
   };
 
   return (
